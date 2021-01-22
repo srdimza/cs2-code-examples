52b1f7fb05ebe70eeae895d2f66dbd913daa4e23
Gundega Dekena <gundega.dekena@gmail.com>
2021-01-22 18:36:27 +0200

    add BruceBanner state examples

---
+++ src/main/week5/hulk/states/DrBanner.scala
@@ -0,0 +1,22 @@
+package week5.hulk.states
+import week5.hulk._
+
+class DrBanner(banner: BruceBanner) extends State(banner) {
+
+  override def makeAngry(): Unit = {
+    banner.state = new TheHulk(banner)
+  }
+
+  override def calmDown(): Unit = {
+    println("already calm")
+  }
+
+  override def useCar(car: Car): Unit = {
+    car.drive(false)
+  }
+
+  override def fight(): Unit = {
+    println("this won't end well")
+  }
+
+}
---
+++ src/main/week5/hulk/states/ProfessorHulk.scala
@@ -0,0 +1,22 @@
+package week5.hulk.states
+import week5.hulk._
+
+class ProfessorHulk(banner: BruceBanner) extends State(banner){
+
+  override def makeAngry(): Unit = {
+    println("No problem")
+  }
+
+  override def calmDown(): Unit = {
+    println("Already calm")
+  }
+
+  override def useCar(car: Car): Unit = {
+    car.drive(true)
+  }
+
+  override def fight(): Unit = {
+    println("Smash carefully")
+  }
+
+}
