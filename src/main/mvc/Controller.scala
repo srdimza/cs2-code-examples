458df77a69388b4f95f27f5fdcf14555395d4881
Gundega Dekena <gundega.dekena@gmail.com>
2021-02-08 12:09:45 +0200

    add mvc example

---
+++ src/main/mvc/Controller.scala
@@ -0,0 +1,11 @@
+package mvc
+
+import scalafx.beans.property.StringProperty
+
+
+class Controller(inputDisplay: StringProperty, model: Model) extends EventHandler[ActionEvent] {
+
+  override def handle(event: ActionEvent): Unit = {}
+
+
+}
